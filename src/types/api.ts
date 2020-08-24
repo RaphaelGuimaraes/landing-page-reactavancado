aeb23d69a4d7ba715aff497b849833fb38eaac1e
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-08-24 18:06:17 -0300

    Make section faq dynamic

--- src/types/api.ts
+++ src/types/api.ts
@@ -36,6 +36,11 @@ export type Review = {
   }
 }
 
+export type Question = {
+  question: string
+  answer: string
+}
+
 export type Image = {
   alternativeText: string
   url: string
@@ -100,6 +105,11 @@ export type SectionReviewsProps = {
   reviews: Review[]
 }
 
+export type SectionFaqProps = {
+  title: string
+  questions: Question[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -111,4 +121,5 @@ export type LandingPageProps = {
   pricingBox: PricingBoxProps
   sectionAboutUs: SectionAboutUsProps
   sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
