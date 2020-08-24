d474147025fa5318a76db7be63f8c40d3c21821d
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-08-24 16:41:44 -0300

    Make section about us dynamic

--- src/types/api.ts
+++ src/types/api.ts
@@ -5,20 +5,35 @@ export type TechIcon = {
   }
 }
 
-export type Concepts = {
+export type Concept = {
   title: string
 }
 
-export type Modules = {
+export type Module = {
   title: string
   subtitle: string
   description: string
 }
 
-export type LogoProps = {
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  photo: Image
+  name: string
+  role: string
+  socialLinks: SocialLink[]
+  description: string
+}
+
+export type Image = {
   alternativeText: string
   url: string
 }
+
+export type LogoProps = Image
 
 export type HeaderProps = {
   title: string
@@ -27,19 +42,13 @@ export type HeaderProps = {
     label: string
     url: string
   }
-  image: {
-    alternativeText: string
-    url: string
-  }
+  image: Image
 }
 
 export type SectionAboutProjectProps = {
   title: string
   description: string
-  image: {
-    alternativeText: string
-    url: string
-  }
+  image: Image
 }
 
 export type SectionTechProps = {
@@ -49,12 +58,12 @@ export type SectionTechProps = {
 
 export type SectionConceptsProps = {
   title: string
-  concepts: Concepts[]
+  concepts: Concept[]
 }
 
 export type SectionModulesProps = {
   title: string
-  modules: Modules[]
+  modules: Module[]
 }
 
 export type SectionAgendaProps = {
@@ -73,6 +82,11 @@ export type PricingBoxProps = {
   }
 }
 
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -82,4 +96,5 @@ export type LandingPageProps = {
   sectionModules: SectionModulesProps
   sectionAgenda: SectionAgendaProps
   pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
 }
